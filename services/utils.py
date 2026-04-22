fbe4d6b59587aa5cef7c01ba295fbd61713017ab
Nechama Glustein <nechamaskorski@gmail.com>
2026-04-22 11:34:14 -0400

    clean up PDF name

--- services/utils.py
+++ services/utils.py
@@ -1,4 +1,10 @@
 import re
+import urllib.parse
+
+def decode_filename(url):
+    """Extract and decode a filename from a URL."""
+    filename = url.split("/")[-1].split("?")[0]
+    return urllib.parse.unquote(filename)
 
 def strip_html(text):
     """Strip HTML tags from a string."""
